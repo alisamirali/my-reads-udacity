ccf1c4f7978626d2e9d506dad89ed45aa2fad06a
Ali Samir <alisamir.2020@azhar.edu.eg>
2022-06-07 20:30:18 +0200

    Updates

---
+++ precache-manifest.b0c90778bfcbac0a2010a6206e1296f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "af3786171e0b8d2082de",
+    "url": "/myreads-app/static/css/main.f6bec686.chunk.css"
+  },
+  {
+    "revision": "af3786171e0b8d2082de",
+    "url": "/myreads-app/static/js/main.af378617.chunk.js"
+  },
+  {
+    "revision": "d59916398aad5ed5d0e3",
+    "url": "/myreads-app/static/js/1.d5991639.chunk.js"
+  },
+  {
+    "revision": "1500c43aa4a67be84717",
+    "url": "/myreads-app/static/js/runtime~main.1500c43a.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myreads-app/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myreads-app/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/myreads-app/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "9b1b0bdde3b28fe8652fdcf70c019113",
+    "url": "/myreads-app/index.html"
+  }
+];
